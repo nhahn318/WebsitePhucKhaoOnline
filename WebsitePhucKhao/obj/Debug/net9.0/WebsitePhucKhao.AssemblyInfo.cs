2131e7d6161d416ae0036955f491cdfb512a1b01
wangvinh710 <145040523+Vinhcoder107@users.noreply.github.com>
2025-04-02 09:38:56 +0700

    Sửa lỗi đăng xuất và chuyển hướng về trang chủ

--- WebsitePhucKhao/obj/Debug/net9.0/WebsitePhucKhao.AssemblyInfo.cs
+++ WebsitePhucKhao/obj/Debug/net9.0/WebsitePhucKhao.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebsitePhucKhao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31a69479ee973c719ca779f7c68a35aa71930c4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d499ce58e4abd355303e420fca58071100aa645")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebsitePhucKhao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebsitePhucKhao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
